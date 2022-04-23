011882af846d62aad5ac46e31f85d905ba1159cc
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-04-23 15:55:41 -0300

    pagina de tarefas, com botao de voltar

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,16 +29,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  private
-
-  def task_params
-    params.require(:task).permit(:title, :details, :completed)
-  end
-
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
